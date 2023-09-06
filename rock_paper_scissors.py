768dc4750e4c2a00a37196854eff0f17cc442ac4
mjlarson2 <mjlarson2@cps.edu>
2023-09-06 10:59:26 -0500

    Implemented get_choices and evaluation

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,30 @@
 # We will write a rock paper scissors game in class - Complete it in this file
+import random
+player_choice = "rock"
+computer_choice = "paper"
+win = "You win!"
+lose = "You lose."
+
+# Function that retrieves the choices
+def get_choices():
+    player_choice = input("Enter choice:")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+
+    return choices
+
+def evaluation(choices:dict):
+    player = choices["player_choice"]
+    com = choices["computer_choice"]
+    if player == com: print("Tie")
+    elif player == "rock":
+        if com == "paper": print(lose)
+        elif com == "scissors": print(win)
+    elif player == "paper":
+        if com == "rock": print(win)
+        elif com == "scissors": print(lose)
+    elif player == 'scissors':
+        if com == "rock": print(lose)
+        elif com == "scissors": print(win)
+        
