b0d738fd451406d0cd661e1632fbaa12a90321aa
mjlarson2 <mjlarson2@cps.edu>
2023-09-11 10:07:13 -0500

    No clue

--- a1_ai_problems.py
+++ a1_ai_problems.py
@@ -29,6 +29,18 @@ def toDecimal(binary:str):
     return decimal
 
 
+# Doesn't work and I don't necessarily feel like bothering to make it do so
+# def toBinary(decimal:int):
+#     binary = "0"
+#     while(toDecimal(binary) != decimal):
+#         for i in range (len(binary)):
+#             if(toDecimal(binary) == decimal): break
+#             if (binary[i:i - 1: -1] == 0): binary[i:i - 1: -1] = "1"
+            
+#         binary = "0" + binary
+                
+
+
 assert toDecimal("00000001") == 1
 assert toDecimal("10010110") == 150
 print("All tests passed!")
